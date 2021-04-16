1ddcc4b7d1b8ac852bc72c169d30e748145f3719
gigagibi <gii01@mail.ru>
2021-04-16 22:39:49 +0300

    updated models.py

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# цифро
 class Internship(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование стажировки')
     emp_company = models.ForeignKey('EmpCompany', verbose_name='Работодатель', on_delete=models.CASCADE)
@@ -9,14 +9,12 @@ class Internship(models.Model):
     input_emp_competence = models.ManyToManyField('EmpCompetence', related_name='input_emp_competence')
     output_emp_competence = models.ManyToManyField('EmpCompetence', related_name='output_emp_competence')
 
-
 class Practice(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование стажировки')
     ed_organization = models.ForeignKey('EdOrganization', verbose_name='Образовательная организация', on_delete=models.CASCADE)
     description = models.TextField(verbose_name='Описание практики')
     input_ed_competence = models.ManyToManyField('EdCompetence', related_name='input_ed_competence')
     output_ed_competence = models.ManyToManyField('EdCompetence', related_name='output_ed_competence')
-
 
 class EdOrganization(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название образовательной организации')
@@ -31,5 +29,7 @@ class EmpCompetence(models.Model):
 
 class EmpCompany(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название организации-работодателя')
-    
+
+class Skills(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название навыка')
     
