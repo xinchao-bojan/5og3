fbd0270ec1334735096d1276644f541fb4333b8c
Ismail Gudaev <70891118+gigagibi@users.noreply.github.com>
2021-04-16 20:39:52 +0300

    Update models.py

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
 # Create your models here.
-Стажировка
-кмопетенции
-образовательная органиазция
-Работа
+
 class Internship(models.Model):
     name = models.CharField(max_length=127, verbose_name='Наименование стажировки')
     emp_company = models.ForeignKey('EmpCompany', verbose_name='Работодатель', on_delete=models.CASCADE)
 
 
-
 class EdOrganization(models.Model):
-    name = models.CharField(max_length=127, verbose_name='Название организации')
+    name = models.CharField(max_length=127, verbose_name='Название образовательной организации')
+    address = models.CharField(max_length=127, verbose_name='Адрес образовательной организации')
 
-class Competence(model.Model):
+class EdCompetence(model.Model):
+    name = models.CharField(max_length=127, verbose_name='Название компетенции')
+    EmpCompetence = models.ManyToManyField()
 
+class EmpCompetence(model.Model):
+    name = models.CharField(max_length=127, verbose_name='Название компетенции')
+    
 class EmpCompany(models.Model):
+    name = models.CharField(max_length=127, verbose_name='Название организации-работодателя')
+    address = models.CharField(max_length=127, verbose_name='Адрес организации-работодателя')
+    
+    
