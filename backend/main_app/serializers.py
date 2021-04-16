5a57de7bb9ea0693b4a3160278c01a7edd9954c7
gigagibi <gii01@mail.ru>
2021-04-16 23:15:52 +0300

    updated serializers.py

--- backend/main_app/serializers.py
+++ backend/main_app/serializers.py
@@ -6,35 +6,42 @@ class EdOrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = EdOrganization
+        fields = '__all__'
 
 class EmpCompanySerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = EmpCompany
+        fields = '__all__'
 
 class EmpCompetenceSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = EmpCompetence
+        fields = '__all__'
 
 class EdCompetenceSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = EdCompetence
+        fields = '__all__'
 
 class InternshipSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = Internship
+        fields = '__all__'
 
 class PracticeSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = Practice
+        fields = '__all__'
 
 class SkillsSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = Skills
+        fields = '__all__'
 
 
--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework import generics, status, permissions
+
+class ListEdOrganizationView(APIView):
+
+class AddEdOrganizationView(APIView):
+
+class DeleteEdOrganizationView(APIView):
+
+class UpdateEdOrganizationView(APIView):
+
+
+
+class ListEmpOrganizationView(APIView):
+
+class AddEmpOrganizationView(APIView):
+
+class DeleteEmpOrganizationView(APIView):
+
+class UpdateEmpOrganizationView(APIView):
+
+
+
+class ListPracticeOrganizationView(APIView):
+
+class AddOrganizationView(APIView):
+
+class DeleteEdOrganizationView(APIView):
+
+class UpdateEdOrganizationView(APIView):
+
+
