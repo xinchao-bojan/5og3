6683d2fc25c6704dc03ef2461005e3adb557ccf9
gigagibi <gii01@mail.ru>
2021-04-18 02:11:53 +0300

    updated serializers.py

--- backend/main_app/serializers.py
+++ backend/main_app/serializers.py
@@ -1,24 +1,52 @@
 from rest_framework import serializers
 from .models import *
-
+from ..custom_user.models import StudentMore, CustomUser
 
 class StudentMSerializer(object):
     pass
+
+
 class PracticeSerializer(object):
     pass
+
+
 class EdCompetenceSerializer(object):
     pass
+
+
 class InternshipSerializer(object):
     pass
+
+
+class CustomUserOnlyNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        depth = 1
+        model = CustomUser
+        fields = ['name']
+
+
+class StudentMoreOnlyUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        depth = 1
+        model = StudentMore
+        fields = ['user']
+
 
 class EdOrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = EdOrganization
         fields = ['id', 'name', 'competence', 'studentm_set', 'practice_set']
-        
-        studentm_set = StudentMSerializer()
-        practice_set = PracticeSerializer()
+
+    studentm_set = StudentMSerializer()
+    practice_set = PracticeSerializer()
+
+
+class EdOrganizationOnlyNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        depth = 1
+        model = EdOrganization
+        fields = ['id', 'name']
 
 
 class EmpCompanySerializer(serializers.ModelSerializer):
@@ -27,7 +55,14 @@ class EmpCompanySerializer(serializers.ModelSerializer):
         model = EmpCompany
         fields = ['id', 'name', 'internship_set', 'rate']
 
-        internship_set = InternshipSerializer()
+    internship_set = InternshipSerializer()
+
+
+class EmpCompanyOnlyNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        depth = 1
+        model = EmpCompany
+        fields = ['id', 'name']
 
 
 class EmpCompetenceSerializer(serializers.ModelSerializer):
@@ -36,7 +71,7 @@ class EmpCompetenceSerializer(serializers.ModelSerializer):
         model = EmpCompetence
         fields = ['id', 'name', 'ed_competence']
 
-        ed_competence = EdCompetenceSerializer()
+    ed_competence = EdCompetenceSerializer()
 
 
 class EdCompetenceSerializer(serializers.ModelSerializer):
@@ -45,17 +80,18 @@ class EdCompetenceSerializer(serializers.ModelSerializer):
         model = EdCompetence
         fields = ['id', 'name', 'emp_competence']
 
-        emp_competence = EmpCompetenceSerializer()
+    emp_competence = EmpCompetenceSerializer()
 
 
 class InternshipSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = Internship
-        fields = ['id', 'name', 'emp_company__name', 'description', 'input_emp_competence', 'output_emp_competence']
+        fields = ['id', 'name', 'emp_company', 'description', 'input_emp_competence', 'output_emp_competence']
 
-        input_emp_competence = EmpCompetenceSerializer()
-        output_emp_competence = EmpCompetenceSerializer()
+    emp_company = EmpCompanyOnlyNameSerializer()
+    input_emp_competence = EmpCompetenceSerializer()
+    output_emp_competence = EmpCompetenceSerializer()
 
 
 class PracticeSerializer(serializers.ModelSerializer):
@@ -64,8 +100,9 @@ class PracticeSerializer(serializers.ModelSerializer):
         model = Practice
         fields = ['id', 'name', 'ed_organization', 'description', 'input_ed_competence', 'output_ed_competence']
 
-        input_ed_competence = EdCompetenceSerializer()
-        output_ed_competence = EdCompetenceSerializer()
+    ed_organization = EdOrganizationOnlyNameSerializer()
+    input_ed_competence = EdCompetenceSerializer()
+    output_ed_competence = EdCompetenceSerializer()
 
 
 class SkillSerializer(serializers.ModelSerializer):
@@ -74,18 +111,19 @@ class SkillSerializer(serializers.ModelSerializer):
         model = Skill
         fields = ['id', 'text', 'user']
 
-        user = StudentMSerializer()
+    user = StudentMSerializer()
 
 
 class StudentMSerializer(serializers.ModelSerializer):
     class Meta:
         depth = 1
         model = StudentM
-        fields = ['id', 'user__user__first_name', 'user__user__last_name', 'user__user__email', 'ed_organization', 'ed_competence', 'emp_competence', 'rate']
+        fields = ['id', 'user__user__first_name', 'user__user__last_name', 'user__user__email', 'ed_organization',
+                  'ed_competence', 'emp_competence', 'rate']
 
-        ed_organization = EdOrganizationSerializer()
-        ed_competence = EdCompetenceSerializer()
-        emp_competence = EmpCompetenceSerializer()
+    ed_organization = EdOrganizationSerializer()
+    ed_competence = EdCompetenceSerializer()
+    emp_competence = EmpCompetenceSerializer()
 
 
 class EdWorkerMSerializer(serializers.ModelSerializer):
@@ -94,7 +132,7 @@ class EdWorkerMSerializer(serializers.ModelSerializer):
         model = EdWorkerM
         fields = ['id', 'user__user__first_name', 'user__user__last_name', 'user__user__email', 'ed_organization']
 
-        ed_organization = EdOrganizationSerializer()
+    ed_organization = EdOrganizationSerializer()
 
 
 class EmployerMSerializer(serializers.ModelSerializer):
@@ -103,7 +141,7 @@ class EmployerMSerializer(serializers.ModelSerializer):
         model = EmployerM
         fields = ['id', 'user__user__first_name', 'user__user__last_name', 'user__user__email', 'emp_company']
 
-        emp_company = EmpCompanySerializer()
+    emp_company = EmpCompanySerializer()
 
 
 class InternshipApplicationSerializer(serializers.ModelSerializer):
@@ -112,6 +150,7 @@ class InternshipApplicationSerializer(serializers.ModelSerializer):
         model = InternshipApplication
         fields = ['id', 'ed_organization', 'internship', 'student', 'accepted']
 
-        ed_organization = EdOrganizationSerializer()
-        internship = InternshipSerializer()
-        student = StudentMSerializer()
+    ed_organization = EdOrganizationSerializer()
+    internship = InternshipSerializer()
+    student = StudentMSerializer()
+
