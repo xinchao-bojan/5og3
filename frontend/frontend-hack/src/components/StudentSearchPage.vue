bcf9d6b40bcf995c49dbc767871903b75de0f598
Ekaterina Gibova <katyagibova@bk.ru>
2021-04-18 02:41:42 +0300

    added search

--- frontend/frontend-hack/src/components/StudentSearchPage.vue
+++ frontend/frontend-hack/src/components/StudentSearchPage.vue
@@ -9,18 +9,20 @@
             <v-row>
                 <v-col cols="5">
                     <v-text-field
-                        v-model="message4"
+                        v-model="searchValue"
                         label="Поиск"
                         outlined
-                        class="text_field"                        
+                        class="text_field"                      
                     ></v-text-field>
                 </v-col>
                 <v-col cols="2">
                     <v-btn
+                    @click="searchStudent"
                     outlined
                     rounded
                     min-height="55px"
-                    class="btn">
+                    class="btn"
+                    >
                     Найти
                     </v-btn>                         
                 </v-col>                        
@@ -29,6 +31,14 @@
                     
                 </v-col> 
                 <v-spacer></v-spacer>               
+            </v-row>
+            <v-row>
+                <p>{{searchValue}}</p>
+                <ul>
+                    <li v-for="st in this.searchStudent" :key="st.id">
+                        <p>{{st.name}}</p>
+                    </li>
+                </ul>
             </v-row>
             <v-row>
                 <StudentCard />
@@ -53,9 +63,68 @@ export default {
   data () {
     return {
         page: 1,
+        searchValue:'',
+        student:[
+            {
+                id: 1,
+                name: "Katya",
+                skills: ["c++", "java", "react", "html"]
+            },
+            {
+                id: 2,
+                name: "Vanya",
+                skills: ["c++", "java", "vue", "html", "git"]
+            },
+            {
+                id: 3,
+                name: "Kostya",
+                skills: ["c++", "java", "pyton", "django"]
+            },
+            {
+                id: 4,
+                name: "Max",
+                skills: ["c++", "java", "pyton", "figma"]
+            },
+            {
+                id: 5,
+                name: "Roma",
+                skills: ["java", "react", "html", "figma", "c#", "c"]
+            },
+            {
+                id: 6,
+                name: "Alica",
+                skills: ["c"]
+            },
+            {
+                id: 7,
+                name: "GeyOrgiy",
+                skills: ["dog", "cat", "drink"]
+            },
+            {
+                id: 8,
+                name: "Дима",
+                skills: ["c++", "pyton"]
+            },
+            {
+                id: 9,
+                name: "Olga",
+                skills: ["time-managment"]
+            },
+            {
+                id: 10,
+                name: "Serega",
+                skills: ["c++", "java"]
+            },
+        ]        
     }
-  }
-    
+  },
+  computed:{
+      searchStudent(){
+          return this.student.filter(el =>{
+              return el.name.toLowerCase().includes(this.searchValue.toLowerCase())
+          })
+      }
+  }    
 }
 </script>
 
